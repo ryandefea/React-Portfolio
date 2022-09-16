ba37f64e5fa669069e2e64a4b469717d29e98094
ryandefea <ryan.defea@gmail.com>
2022-09-16 17:34:43 -0500

    fix class name

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ import CTA from './CTA';
 const Header = () => {
   return (
     <header>
-      <div className=".container.header__container">
+      <div className="container.header__container">
         <h5>Hello World,</h5>
         <h1>I am Ryan DeFea</h1>
         <h5 className="text-light">Fullstack Web Developer</h5>

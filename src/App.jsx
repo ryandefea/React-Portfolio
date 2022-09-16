b4f63e0224b685ce857112f84e5d08647fd7ea3b
ryandefea <ryan.defea@gmail.com>
2022-09-16 13:15:15 -0500

    add file import statements and files to be rendered in fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Nav from './components/nav/Nav';
+import Header from './components/header/Header';
+import About from './components/about/About';
+import Experience from './components/experience/Experience';
+import Services from './components/services/Services';
+import Portfolio from './components/portfolio/Portfolio';
+import Testimonials from './components/testimonials/Testimonials';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
 const App = () => {
   return (
     <React.Fragment>
-      
+      <Nav />
+      <Header />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </React.Fragment>
   )
 }

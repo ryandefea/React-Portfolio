6ecf6cc0e157501596b14921ec2532120857cf01
ryandefea <ryan.defea@gmail.com>
2022-09-20 17:41:45 -0500

    add links to socials

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import {BsLinkedin} from 'react-icons/bs';
 
 const HeaderSocials = () => {
   return (
-    <div>HeaderSocials</div>
+    <div className='headerSocials'>
+      <a href="https://www.linkedin.com/in/ryan-defea/" target=" _blank"><BsLinkedin/></a>
+      <a href="https://github.com/ryandefea" target=" _blank"></a>
+    </div>
   )
 }
 

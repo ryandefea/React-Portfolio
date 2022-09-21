016c8280629816e6f9ae4201b22d85f60fa79e5e
ryandefea <ryan.defea@gmail.com>
2022-09-20 19:21:43 -0500

    add header icon

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {BsLinkedin} from 'react-icons/bs';
 import {BsGithub} from 'react-icons/bs';
+import {BsController} from 'react-icons/bs';
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
       <a href="https://www.linkedin.com/in/ryan-defea/" target=" _blank"><BsLinkedin/></a>
       <a href="https://github.com/ryandefea" target=" _blank"><BsGithub/></a>
+      <a href="https://cod.tracker.gg/warzone/profile/atvi/flyinryan_d%237360883/overview?mode=br" target=" _blank"><BsController/></a>
+
     </div>
   )
 }

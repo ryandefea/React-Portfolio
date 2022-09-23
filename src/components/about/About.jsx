0bba3608f04dc8e1244f6226292d288765f8728b
ryandefea <ryan.defea@gmail.com>
2022-09-23 13:04:21 -0500

    comment out about me section for now, i think i will combine this with header info

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,27 @@ import './about.css';
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      {/* <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src="" alt="" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+
+
+            </article>
+          </div>
+        </div>
+      </div> */}
+      </section>
   )
 }
 

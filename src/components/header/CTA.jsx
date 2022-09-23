43dd833b68ffaac7fa68c5eba1f47ed78f2c35bf
ryandefea <ryan.defea@gmail.com>
2022-09-23 13:04:49 -0500

    change button class to match other button

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import Resume from '../../assets/Ryan DeFea- Resume.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-      <a href="#contact" className='btn btn-primary'>Contact Me</a>
+      <a href="#contact" className='btn'>Contact Me</a>
       <a href={Resume} download className='btn'>Download Resume</a>
     </div>
   )

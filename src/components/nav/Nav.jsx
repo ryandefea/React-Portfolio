258d01eb042333b04ecea8da8e41df5cf0b6cc88
ryandefea <ryan.defea@gmail.com>
2022-09-22 20:53:59 -0500

    begin adding hooks to toggle which section is selected

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import './nav.css';
+import {AiOutlineHome} from 'react-icons/ai';
+import {GiThink} from 'react-icons/gi';
+import {SiFuturelearn} from 'react-icons/si';
+import {FcServices} from 'react-icons/fc';
+import {AiFillPhone} from 'react-icons/ai';
+import {useState} from 'react';
+
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')}  className={activeNav === '#' ? 'active' : '' }><AiOutlineHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><GiThink/></a>
+      <a href="#experience"><SiFuturelearn/></a>
+      <a href="#services"><FcServices/></a>
+      <a href="#contact"><AiFillPhone/></a>
+    </nav>
   )
 }
 

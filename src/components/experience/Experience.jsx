92f4dfd43a3af7c93a187159bcee46536fbba946
ryandefea <ryan.defea@gmail.com>
2022-09-30 12:35:00 -0500

    import react icons and resize

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import './experience.css';
+import {DiBootstrap} from 'react-icons/di';
+import {SiCsharp} from 'react-icons/si';
+import {DiCss3} from 'react-icons/di';
+import {DiHtml5} from 'react-icons/di';
+import {DiJsBadge} from 'react-icons/di';
+import {DiMysql} from 'react-icons/di';
+import {DiPython} from 'react-icons/di';
+import {DiReact} from 'react-icons/di';
+import {DiNodejs} from 'react-icons/di';
 
 const Experience = () => {
   return (
@@ -9,34 +18,33 @@ const Experience = () => {
       <div className="container experience__container">
         <div className="experience__content">
           <article className="experience__details">
-
+            <DiBootstrap size={"3.5em"} />
           </article>
           <article className="experience__details">
-
+            <SiCsharp size={"3em"} />
           </article>
           <article className="experience__details">
-
+            <DiCss3 size={"3em"} />
           </article>
           <article className="experience__details">
-
+            <DiHtml5 size={"3em"} />
           </article>
           <article className="experience__details">
-
+            <DiJsBadge size={"3em"} />
           </article>
           <article className="experience__details">
-
+            <DiMysql size={"3em"} />
           </article>
           <article className="experience__details">
-
+            <DiPython size={"3em"} />
           </article>
           <article className="experience__details">
-
+            <DiReact size={"3em"} />
           </article>
           <article className="experience__details">
-
+            <DiNodejs size={"3em"} />
           </article>
         </div>
-
       </div>
 
     

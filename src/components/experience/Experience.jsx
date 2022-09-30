eaaa724db0a0e7b652e1d090b814e3d355715d1f
ryandefea <ryan.defea@gmail.com>
2022-09-30 11:12:05 -0500

    create jsx layout for programming experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -3,7 +3,44 @@ import './experience.css';
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What I Have Learned</h5>
+      <h2>Programming Languages</h2>
+      <div className="container experience__container">
+        <div className="experience__content">
+          <article className="experience__details">
+
+          </article>
+          <article className="experience__details">
+
+          </article>
+          <article className="experience__details">
+
+          </article>
+          <article className="experience__details">
+
+          </article>
+          <article className="experience__details">
+
+          </article>
+          <article className="experience__details">
+
+          </article>
+          <article className="experience__details">
+
+          </article>
+          <article className="experience__details">
+
+          </article>
+          <article className="experience__details">
+
+          </article>
+        </div>
+
+      </div>
+
+    
+    </section>
   )
 }
 

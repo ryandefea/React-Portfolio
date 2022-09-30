7ba2c24055ae131c1beeb2588a83163f00a04492
ryandefea <ryan.defea@gmail.com>
2022-09-30 10:11:02 -0500

    remove picture from header file, this file will be removed altogether eventually

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './header.css';
 import  HeaderSocials from './HeaderSocials';
 import CTA from './CTA';
-import RYAN from '../../assets/RYAN.JPG'
+
 
 const Header = () => {
   return (
@@ -16,7 +16,7 @@ const Header = () => {
         <HeaderSocials /> 
         <div className="about__me">
         <div className="ryan">
-          <img src={RYAN} alt="me" />
+          
         </div>
         </div>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.

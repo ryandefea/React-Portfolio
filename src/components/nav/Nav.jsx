49d9e73511c134b116234fca23aa25327aa7f917
ryandefea <ryan.defea@gmail.com>
2022-09-30 10:10:15 -0500

    fix onClick spelling

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -16,7 +16,7 @@ const Nav = () => {
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><GiThink/></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><SiFuturelearn/></a>
       <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><FcServices/></a>
-      <a href="#contact" on Click={() => setActiveNav('#contact')} className={activeNav ===  '#contact' ? 'active' : ''}><AiFillPhone/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav ===  '#contact' ? 'active' : ''}><AiFillPhone/></a>
     </nav>
   )
 }

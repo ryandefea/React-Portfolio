2153edd501ff7cc7736b53b343df2b13cbbf9628
ryandefea <ryan.defea@gmail.com>
2022-10-02 15:13:36 -0500

    new navbar is in place, buggy in mobile though

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './nav.css';
+import { useRef } from "react";
 import {AiOutlineHome} from 'react-icons/ai';
 import {GiThink} from 'react-icons/gi';
 import {SiFuturelearn} from 'react-icons/si';
@@ -9,16 +10,57 @@ import {useState} from 'react';
 
 
 const Nav = () => {
-  const [activeNav, setActiveNav] = useState('#')
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  }
   return (
-    <nav>
-      <a href="#" onClick={() => setActiveNav('#')}  className={activeNav === '#' ? 'active' : '' }><AiOutlineHome/></a>
-      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><GiThink/></a>
-      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><SiFuturelearn/></a>
-      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><FcServices/></a>
-      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav ===  '#contact' ? 'active' : ''}><AiFillPhone/></a>
+    <header className="newHeader">
+    <nav ref={navRef}>
+      <a href="#">Home</a>
+      <a href="#about">About</a>
+      <a href="#experience">Experience</a>
+      <a href="#services">Services</a>
+      <a href="#contact">Contact</a>
+      <button className="nav-btn nav-close-btn" onCLick={showNavbar}>
+        <FcServices />
+      </button>
     </nav>
-  )
+    <button className="nav-btn" onClick={showNavbar}>
+      <AiFillPhone />
+    </button>
+    </header>
+  );
 }
 
 export default Nav;
+
+//original navbar 
+
+
+
+// import React from 'react';
+// import './nav.css';
+// import {AiOutlineHome} from 'react-icons/ai';
+// import {GiThink} from 'react-icons/gi';
+// import {SiFuturelearn} from 'react-icons/si';
+// import {FcServices} from 'react-icons/fc';
+// import {AiFillPhone} from 'react-icons/ai';
+// import {useState} from 'react';
+
+
+// const Nav = () => {
+//   const [activeNav, setActiveNav] = useState('#')
+//   return (
+//     <nav>
+//       <a href="#" onClick={() => setActiveNav('#')}  className={activeNav === '#' ? 'active' : '' }><AiOutlineHome/></a>
+//       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><GiThink/></a>
+//       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><SiFuturelearn/></a>
+//       <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><FcServices/></a>
+//       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav ===  '#contact' ? 'active' : ''}><AiFillPhone/></a>
+//     </nav>
+//   )
+// }
+
+// export default Nav;

3b5a7430a462e6b9e503cb2327d7916990a20a29
ryandefea <ryan.defea@gmail.com>
2022-10-04 20:56:53 -0500

    update nav bar still kind of busted with logo

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,46 +1,44 @@
-import React from 'react';
-import './nav.css';
+import React from "react";
+import "./nav.css";
 import { useRef } from "react";
-import {AiOutlineHome} from 'react-icons/ai';
-import {GiThink} from 'react-icons/gi';
-import {SiFuturelearn} from 'react-icons/si';
-import {FcServices} from 'react-icons/fc';
-import {AiFillPhone} from 'react-icons/ai';
-import {useState} from 'react';
-import {FaBars, FaTimes} from 'react-icons/fa';
-
-
+import { AiOutlineHome } from "react-icons/ai";
+import { GiThink } from "react-icons/gi";
+import { SiFuturelearn } from "react-icons/si";
+import { FcServices } from "react-icons/fc";
+import { AiFillPhone } from "react-icons/ai";
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+// import RD from "../../assets/RD.svg";
 
 const Nav = () => {
   const navRef = useRef();
 
   const showNavbar = () => {
     navRef.current.classList.toggle("responsive_nav");
-  }
+  };
   return (
     <header className="newHeader">
-    <nav ref={navRef}>
-      <a href="#">Home</a>
-      <a href="#about">About</a>
-      <a href="#experience">Experience</a>
-      <a href="#portfolio">Projects</a>
-      <a href="#contact">Contact</a>
-      <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-        <FaTimes/>
+      {/* <img className="logo" src={RD} alt="rd" /> */}
+      <nav ref={navRef}>
+        <a href="#">Home</a>
+        <a href="#about">About</a>
+        <a href="#experience">Experience</a>
+        <a href="#portfolio">Projects</a>
+        <a href="#contact">Contact</a>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars />
       </button>
-    </nav>
-    <button className="nav-btn" onClick={showNavbar}>
-      <FaBars/>
-    </button>
     </header>
   );
-}
+};
 
 export default Nav;
 
-//original navbar 
-
-
+//original navbar
 
 // import React from 'react';
 // import './nav.css';
@@ -50,7 +48,6 @@ export default Nav;
 // import {FcServices} from 'react-icons/fc';
 // import {AiFillPhone} from 'react-icons/ai';
 // import {useState} from 'react';
-
 
 // const Nav = () => {
 //   const [activeNav, setActiveNav] = useState('#')

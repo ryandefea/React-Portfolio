087e7bc1af66ecf9a8ccb93816be833ac143a1c8
ryandefea <ryan.defea@gmail.com>
2022-10-04 20:59:36 -0500

    move links to footer from header side thingy

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './footer.css';
+import {BsLinkedin} from 'react-icons/bs';
+import {BsGithub} from 'react-icons/bs';
+import {BsController} from 'react-icons/bs';
 
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>Ryan</a>
+      {/* <a href="#" className='footer__logo'>Ryan</a> */}
 
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
@@ -13,6 +16,12 @@ const Footer = () => {
         <li><a href="#portfolio">Portfolio</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
+
+      <div className="footer__links">
+      <a href="https://www.linkedin.com/in/ryan-defea/" target=" _blank"><BsLinkedin/></a>
+      <a href="https://github.com/ryandefea" target=" _blank"><BsGithub/></a>
+      <a href="https://cod.tracker.gg/warzone/profile/atvi/flyinryan_d%237360883/overview?mode=br" target=" _blank"><BsController/></a>
+      </div>
 
       <div className='footer__copyright'>
         <small>&copy; Ryan DeFea. All rights reserved.</small>
